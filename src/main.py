7d44c67ff2f6370ec4a289bdfc5347d4db63de4f
Luke Texon <luk.tex37@gmail.com>
2019-10-13 23:18:33 +0100

    Implemented dockerisation

--- src/main.py
+++ src/main.py
@@ -7,11 +7,20 @@ auth_filename = 'auth.json'
 
 
 def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="Execute the trend bot.")
+    parser.add_argument("--test", action='store_true')
+    args = parser.parse_args()
+
     auth_path = os.path.join('config', auth_filename)
     tweepy_api = TwitterAuth.create_auth_from_json_file(auth_path)
 
     trend_bot = TrendBot(tweepy_api, MAX_TWEETS_PER_REQUEST)
-    trend_bot.tweet_trending_topic()
+
+    if args.test:
+        trend_bot.tweet_test()
+    else:
+        trend_bot.tweet_trending_topic()
 
 
 if __name__ == '__main__':
